64de866e0add3d1f542f8d8e12a6f650381a430c
SamridhG <samridh1998@gmail.com>
2025-05-25 14:04:10 +0530

    feat: hamburger icon add

--- src/View/Header/components/SearchBar.tsx
+++ src/View/Header/components/SearchBar.tsx
@@ -6,10 +6,10 @@ function SearchBar({isVisibileSiderBar,setVisibilitySideBar}:SearchBarProps) {
   return (
    <div className="flex items-center space-x-4">
    { !isVisibileSiderBar &&
-    <button className="w-10 h-10 bg-[#5030e5] rounded-md flex items-center justify-center cursor-pointer" onClick={
+    <button className="w-10 h-10  rounded-md flex items-center justify-center cursor-pointer" onClick={
         ()=>{setVisibilitySideBar(true)}
     }>
-            <img src="/images/img_pause.svg" alt="Menu" className="w-5 h-5" />
+            <img src="/images/Hamburger.svg" alt="Menu" className="w-5 h-5" />
           </button>
     }
    <div className="w-[417px] h-11 bg-[#f5f5f5] rounded-md flex items-center px-4">
